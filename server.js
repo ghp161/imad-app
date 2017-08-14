cee1996cdf7a593c9053c18c9fa01f3119b49670
ghp161 <ghp161@gmail.com>
2017-08-14 19:00:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,8 @@ function createTemplate(data){
             <title>${title}</title>
             <meta name="viewport" content="width=device-width,initial-scale=1">
            <link href="/ui/style.css" rel="stylesheet" />
+           <script type="text/javascript" src="/ui/main.js">
+        </script>
         </head>
         <body>
         <form id="art">

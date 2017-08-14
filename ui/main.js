1ddcb7e50b8b82d26b7b4708507aab9302bc5786
ghp161 <ghp161@gmail.com>
2017-08-14 19:02:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,8 @@
 console.log('Loaded!');
 
 //change text of using javascript
-var element = document.getElementById("main-text");
-
-element.innerHTML = "Hari Prasad";
+//var element = document.getElementById("main-text");
+//element.innerHTML = "Hari Prasad";
 
 //move image on click
 var img = document.getElementById("madi");
